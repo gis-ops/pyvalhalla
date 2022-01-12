80bb2110e75caf4c5a82ac28df5a01a9fe9c83c8
nilsnolde <nils.nolde@gmail.com>
2022-01-12 14:50:10 +0100

    update valhalla master; remove in-repo conanfile.txt and use valhalla's after all; adapt build scripts

--- scripts/build_linux_do.sh
+++ scripts/build_linux_do.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
+set -e
 
 cd valhalla-py
 
+## some cleanup in case the last build failed or so
+#git -C upstream checkout .
+#rm dist/*
+#rm wheelhouse/*
+
+# install conan
 apt-get update
 apt-get install -y python3-pip
 pip3 install conan
 conan config set "storage.path=$PWD/upstream/conan_data"
-conan install --install-folder upstream/conan_build .
+conan install --install-folder upstream/conan_build upstream/
+
+# apply any patches
+pushd upstream
+git apply ../upstream_patches/*
+popd
+
+# build valhalla
 cmake -B upstream/build -S upstream/ -DENABLE_CCACHE=OFF -DBUILD_SHARED_LIBS=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_PYTHON_BINDINGS=ON -DENABLE_TESTS=OFF -DENABLE_TOOLS=OFF -DENABLE_SERVICES=OFF -DENABLE_HTTP=OFF -DENABLE_CCACHE=OFF -DCMAKE_BUILD_TYPE=Release
 cmake --build upstream/build -- -j$(nproc)
+
+# reset valhalla upstream
+git -C upstream checkout .
 
 echo "copying all headers"
 deps="libcurl4-openssl-dev libgeos++-dev libgeos-dev libluajit-5.1-dev libprotobuf-dev libspatialite-dev libsqlite3-dev"
@@ -27,6 +44,16 @@ for dep in $deps; do
   done
 done
 
+# copy valhalla headers from upstream to lib/common
+rm -r include/common/valhalla
+cp -r upstream/valhalla include/common
+mv include/common/valhalla/config.h.cmake include/common/valhalla/config.h
+mv include/common/valhalla/valhalla.h.in include/common/valhalla/valhalla.h
+
+# copy libvalhalla so it's available for setup.py in its most recent version
+# patch the files names of some headers
+cp -f upstream/build/src/libvalhalla.a lib/linux
+
 # make the bindings so we can see which libraries it exactly links to and also for testing
 /opt/python/cp310-cp310/bin/pip install -r build-requirements.txt
 /opt/python/cp310-cp310/bin/python3 setup.py bdist_wheel
@@ -34,7 +61,7 @@ done
 echo "copying all libraries"
 deps="libprotobuf-lite libcurl libz"
 # prints all linked library paths
-for path in $(ldd build/lib.linux-x86_64-3.9/valhalla/_valhalla.cpython-39-x86_64-linux-gnu.so | awk 'NF == 4 {print $3}; NF == 2 {print $1}'); do
+for path in $(ldd build/lib.linux-x86_64-3.10/valhalla/_valhalla.cpython-310-x86_64-linux-gnu.so | awk 'NF == 4 {print $3}; NF == 2 {print $1}'); do
   for dep in ${deps}; do
     if [[ ${path} == *${dep}* ]]; then
       if [[ ${path} == *"/x86_64-linux-gnu/"* ]]; then
@@ -50,11 +77,10 @@ for path in $(ldd build/lib.linux-x86_64-3.9/valhalla/_valhalla.cpython-39-x86_6
   done
 done
 
-# copy libvalhalla
-cp -f upstream/build/src/libvalhalla.a lib/linux
-
 # build the proto files into the include/linux/valhalla/proto directory
 mkdir -p include/linux/valhalla/proto
-protoc --proto_path=upstream/proto --cpp_out=include/linux/valhalla/proto upstream/proto/*.proto
+/usr/bin/protoc --proto_path=upstream/proto --cpp_out=include/linux/valhalla/proto upstream/proto/*.proto
 
-auditwheel repair --plat manylinux_2_24_x86_64 dist/*
+for so in dist/*; do
+  auditwheel repair --plat manylinux_2_24_x86_64 "${so}"
+done
