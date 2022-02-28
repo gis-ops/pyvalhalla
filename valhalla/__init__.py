7b85c3e364f052d7930f79b77235392a1f7f7cec
nilsnolde <nils.nolde@gmail.com>
2022-02-28 02:26:21 +0100

    trigger build

--- valhalla/__init__.py
+++ valhalla/__init__.py
@@ -2,7 +2,6 @@ from .actor import Actor
 from .config import get_config
 from ._valhalla import *
 from .__version__ import __version__
-from utils import decode_polyline
 
 __version__ = __version__
 __valhalla_commit__ = "b5ce7c418"
