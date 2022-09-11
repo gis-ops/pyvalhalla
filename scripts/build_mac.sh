55c017c5adeca1c7296b864134525ff75f57ee1d
nilsnolde <nilsnolde@proton.me>
2022-09-11 19:24:19 +0200

    argh.. I remember now why we had to symlink, setup.py wouldn't find protobuf-lite otherwise..

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -39,6 +39,7 @@ cp -f upstream/build/src/libvalhalla.a lib/darwin
 
 # copy dependencies
 cp -RL /usr/local/lib/libprotobuf-lite.dylib lib/darwin/libprotobuf-lite.32.dylib
+ln -s libprotobuf-lite.32.dylib libprotobuf-lite.dylib
 
 mkdir -p include/darwin/valhalla/proto
 protoc --proto_path=upstream/proto --cpp_out=include/darwin/valhalla/proto upstream/proto/*.proto
