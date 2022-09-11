cee9196b759da9d12ce230b222de1c3311afeedf
nilsnolde <nilsnolde@proton.me>
2022-09-11 18:19:58 +0200

    locally built protobuf and make sure it's compatible with mac osx 10.9

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -11,14 +11,14 @@ git apply ../upstream_patches/*
 popd
 
 # TODO: the env var can be omitted once geos 3.11 is released: https://github.com/libgeos/geos/issues/500
-cmake -B upstream/build -S upstream/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DENABLE_CCACHE=OFF -DBUILD_SHARED_LIBS=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_PYTHON_BINDINGS=ON -DENABLE_TESTS=OFF -DENABLE_TOOLS=OFF -DENABLE_SERVICES=OFF -DENABLE_HTTP=OFF -DENABLE_CCACHE=OFF -DCMAKE_BUILD_TYPE=Release
+cmake -B upstream/build -S upstream/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DENABLE_CCACHE=OFF -DBUILD_SHARED_LIBS=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_PYTHON_BINDINGS=ON -DENABLE_TESTS=OFF -DENABLE_TOOLS=OFF -DENABLE_DATA_TOOLS=OFF -DENABLE_SERVICES=OFF -DENABLE_HTTP=OFF -DENABLE_CCACHE=OFF -DCMAKE_BUILD_TYPE=Release
 cmake --build upstream/build -- -j$(sysctl -n hw.logicalcpu)
 
 rm -r include/darwin/*
 rm -r lib/darwin
 
-echo "copying all headers"
-deps="curl geos luajit protobuf libspatialite sqlite"
+echo "copying all headers except protobuf"
+deps="curl geos luajit libspatialite sqlite"
 for dep in $deps; do
   for path in $(brew list ${dep} -v); do
     # find and copy the headers
@@ -30,6 +30,8 @@ for dep in $deps; do
     fi
   done
 done
+
+cp -rf /usr/local/include/google include/darwin
 
 # copy libvalhalla
 mkdir -p lib/darwin
